b4af14cb0fddf467926a38a19f816b6a44168fee
darkmatter2048 <darkmatter2048@outlook.com>
2026-06-28 19:16:58 +0800

    新增keam广告脚本并更新忽略规则以优化广告加载

---
+++ public/keam-ad.js
@@ -0,0 +1,10 @@
+(function(keam){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = keam || {};
+s.src = "\/\/quarrelsomebitter.com\/beX.Vks\/dCGwla0tY_WAcW\/FeymA9muRZeUSl-kSPGTicbx\/NvzNYU3jM\/zLM\/tONdzOEs3CNnjtcZzzNHwf";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
